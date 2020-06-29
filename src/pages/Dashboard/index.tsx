4884775a6f878e0c5a09ed8f47ee9cf1a5febe61
Ayrton Souza <ayrtonsouza@icloud.com>
2020-06-29 16:08:52 -0300

    dashboard style development

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,32 @@
 import React from 'react';
-import { Title } from './styles';
+import { FiChevronRight } from 'react-icons/fi';
+import logoImg from '../../assets/logo.svg';
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title> Explore repositórios no Github. </Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title> Explore repositórios no Github. </Title>
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+      <Repositories>
+        <a href="test">
+          <img
+            src="https://avatars0.githubusercontent.com/u/28929274?s=200&v=4"
+            alt="github-profile"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
